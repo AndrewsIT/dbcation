dc6f4f2540ca12150c14c69def26ee7110114cfb
somecallmemike <somecallmemike@gmila.com>
2013-02-13 12:18:17 -0800

    Create vacation.sql

---
+++ vacation.sql
@@ -0,0 +1,36 @@
+--
+-- Database: `vacation`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outofoffice`
+--
+
+CREATE TABLE IF NOT EXISTS `outofoffice` (
+  `username` varchar(64) NOT NULL DEFAULT '',
+  `startdate` datetime DEFAULT NULL,
+  `enddate` datetime DEFAULT NULL,
+  `subject` text,
+  `processed` int(11) DEFAULT '0',
+  `bodytext` text,
+  `ooofrom` varchar(64) DEFAULT NULL,
+  `hostname` text,
+  PRIMARY KEY (`username`),
+  KEY `enddate` (`enddate`),
+  KEY `startdate` (`startdate`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outofoffice_log`
+--
+
+CREATE TABLE IF NOT EXISTS `outofoffice_log` (
+  `recipient` varchar(255) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`created`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
